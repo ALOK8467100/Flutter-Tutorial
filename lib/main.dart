28c6f9bd107ed0d279e01ff3f02b997a3a8c8152
alok <alokkumarcse07@gmail.com>
2023-06-23 10:52:26 +0530

    ch4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_import, avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: unnecessary_import, avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, prefer_final_fields, unused_field
 
 import 'package:flutter/material.dart';
 // import 'package:flutter/widgets.dart';
@@ -17,47 +17,129 @@ void main(){
 }
 
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var myText =  "Change My Name";
+  TextEditingController _nameController = TextEditingController(); 
+
+
+// if we want to run something before this page or widget then we use initState
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  // if we want to destory that perticular screen then we use this
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:  Colors.grey[200],
       appBar: AppBar(
         title: const Text("Awesome App"),
       ),
-      body: Container(
-        height: 500,  // height and row will be changed according with Row and column 
-        // width:300,
-        color:Colors.teal,
-        child: Row( // in place of column we can put Row and see changes.
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment:CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.red,
-          width: 100,
-          height: 100,
-          alignment: Alignment.topLeft,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child:  Column(
+                children: <Widget>[
+                Image.asset(
+                  "assets/images.jpg",
+                  // width: 100,
+                  // height: 100,
+                  // fit: BoxFit.fitWidth,  // we can use fit tag here if required
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText:"Enter Some Text", labelText:"Name" 
+                  ),
+                  ),
+                ),
+              ]
+              ),
+            ),
           ),
-          Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.yellow,
-          width: 100,
-          height: 100,
-          alignment: Alignment.topLeft,
-          ),
-          Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.green,
-          width: 100,
-          height: 100,
-          alignment: Alignment.topLeft,
-          ),
-          ],
         ),
       ), 
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Alok Kumar"), 
+              accountEmail: Text("alokkumarcse67@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+                  ),
+                ),
+            // DrawerHeader(  // this is basic Drawerheader code
+            //   child: Text("Hi I an a Drawer"),
+            //   decoration: BoxDecoration(color:Colors.purple),
+             ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Alok Kumar"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: (){}, // varoius tralling like settings,thumb_up,account_circle,edit etc
+            ),
+             ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("alokkumar67@gmail.com"),
+              trailing: Icon(Icons.edit),
+              onTap:(){}, // varoius tralling like settings,thumb_up,account_circle,edit etc
+            ),
+          ],
+        ),
+      ),
+
+      // floatingActionButton: FloatingActionButton.extended(  // when we use extra varible like text, then use extendedin function
+      //   onPressed: (){},
+      //  label: const Text('Approve'),
+      //  icon: const Icon(Icons.thumb_up),
+      //    backgroundColor: Colors.pink,
+      // ),
+
+      floatingActionButton: FloatingActionButton( // 
+        onPressed: (){
+          myText = _nameController.text;
+          setState(() {
+            
+          });
+        },
+        child: Icon(Icons.send),
+         backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,  // this is used to change the location of our floatingbutton
+
     );
   }
 }
