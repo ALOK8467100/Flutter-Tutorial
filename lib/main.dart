56cba3882ce2687ebfaf5ba5a8d86cd16cb143af
alok <alokkumarcse07@gmail.com>
2023-06-27 18:44:15 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_by_fcc/pages/home_page.dart';
 
-void main(){
+void main() {
   // WidgetsApp // MaterialAppp  // CupertinoApp
   // now WidgetsApp not provide inbuild design, it is kind of low level development.
   // So we use MaterialApp for various functions.
   // And if we want to intrested in Human Interface so we use CupertinoApp
 
-
   // now using again and again hot restart we made a new class of MyApp.
   // and in MyApp we initialized our MaterialApp
   runApp(MyApp());
 }
-
-
 
 // here we declare a new class name as MyApp
 // And in this we assign MaterialAppp
@@ -25,12 +22,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Homepage(),
-     theme:ThemeData(
-      primarySwatch: Colors.purple,
-     ),
-  );
+      home: Homepage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    );
   }
 }
-
-
